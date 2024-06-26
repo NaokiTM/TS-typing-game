a59813c16ffa15a954481918df67f2475a189d75
naoki miyagawa <ntsome21@gmail.com>
2024-06-26 20:58:27 +0100

    installed framer motion, added premade caret component using motion div

--- src/ts/components/Options.tsx
+++ src/ts/components/Options.tsx
@@ -1,5 +1,5 @@
 export default function Options() {
   return (
-    <div>Options</div>
+    <div className="'bg-green-300 text-green-700 flex flex-col items-center h-[70vh] space-y-4 font-mono text-xl p-20">Options</div>
   )
 }
--- src/ts/components/Test.tsx
+++ src/ts/components/Test.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
+import Caret from './caret';
 
 const Test = () => {
 
   const [wordsArray, setWordsArray] = useState<string[]>([])
   const [testString, setTestString] = useState<string>()
   const selectedWordCount = 20;
+  const textFileLength = wordsArray.length
 
+  //generating a initial prompt and defining function for refreshing the prompt using a button
 
   useEffect(() => {
     fetch('../../../words.txt')
@@ -27,26 +30,30 @@ const Test = () => {
 
   const generateNewTest = () => {
   
-    if (wordsArray.length === 0) {
-      console.log("words array is empty")
+    if (textFileLength === 0) {
+      console.log("wordsArray is empty")
       return;
     }
 
-    const newSelectedWords = []
+    const selectedWords = []
 
     for (let i = 0; i < selectedWordCount; i++) {
-      let index = Math.floor(Math.random() * wordsArray.length);
-      newSelectedWords.push(wordsArray[index])
+      let index = Math.floor(Math.random() * textFileLength);
+      selectedWords.push(wordsArray[index])
     }
 
-    setTestString(newSelectedWords.join(" "))
+    setTestString(selectedWords.join(" "))
     return testString
   }
+
+  //when user selects get user input to see how many characters they have typed, make all typed characters appear red
   
 
+
   return (
-    <div className='bg-green-300 text-green-700 flex flex-col items-center justify-center h-[70vh] space-y-4 font-mono text-xl '>
+    <div className='bg-green-300 text-green-700 flex flex-col items-center justify-center h-[70vh] space-y-4 font-mono text-xl p-20'>
       <div>{testString}</div>
+      <Caret />
       <button onClick = {generateNewTest} className=''>refresh the test</button>
     </div> 
   )
