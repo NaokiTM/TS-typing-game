1247a45f04eb99254e3edeae5ed2818369b2a912
NaokiTM <ntsome21@gmail.com>
2024-09-14 03:09:31 +0100

    added tailwind classes to safelist for themes

--- src/ts/hooks/useChangeTheme.tsx
+++ src/ts/hooks/useChangeTheme.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const useChangeTheme = () => {
   // State to hold the current theme class names
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    'bg-emerald-600',
+    'bg-neutral-100',
+    'text-black',
+    'text-neutral-700',
+    'bg-red-600',
+    'bg-black',
+    'from-red-600',
+    'to-black',
+    'from-emerald-600',
+    'to-neutral-100',
+  ],
   theme: {
     extend: {},
   },
